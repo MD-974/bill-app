7510874a32db58cc7e4e3729818df15f7119ad00
MD-974 <davidcolmoulin974@gmail.com>
2024-07-12 16:46:30 +0400

    indentation et ajout fin de commentaire pour la page Bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -21,26 +21,30 @@ describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
   // "Quand je suis sur la page des factures"
 
-
   // ---------------------------------------------------------------------------- //
   //                               TEST ACTIVE ICON                               //
   // ---------------------------------------------------------------------------- //
     test("Then bill icon in vertical layout should be highlighted", async () => {
       // "Alors l'icône de la facture dans la disposition verticale devrait être mise en surbrillance"
+      
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      // Définit un utilisateur de type 'Employee' dans le localStorage
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
-      // Appelle la fonction "router()"
+      // Appelle la fonction "router() pour initialiser le routeur"
       router()
+      // Navigue vers la route des factures
       window.onNavigate(ROUTES_PATH.Bills)
+      // Attend que l'élément avec le data-testid 'icon-window' soit rendu dans le DOM
       await waitFor(() => screen.getByTestId('icon-window'))
+      // Recupère l'element avec le data-testid 'icon-window'
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-      // Vérifie que la classe "active-icon" est ajouté
+      // Vérifie que la classe "active-icon" est ajoutée à l'élément 'windowIcon' 
       expect(windowIcon.classList.contains("active-icon")).toBeTruthy()
     })
 
@@ -49,15 +53,20 @@ describe("Given I am connected as an employee", () => {
   // ---------------------------------------------------------------------------- //
     test("Then bills should be ordered from earliest to latest", () => {
       // "Alors les factures devraient être ordonnées de la plus ancienne à la plus récente"
+      
+      // Ajoute le contenu HTML des factures à la page
       document.body.innerHTML = BillsUI({ data: bills })
 
-      // Extraction des dates des factures affichées "regex"
+      // Extraction des dates des factures affichées à l'aide d'une expression régulière
+      // Récupère tous les éléments de texte correspondant au format de date (yyyy-mm-dd) et extrait leur contenu HTML
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
 
-      // Fonction de tri des dates "plus récente à la plus ancienne
+      // Définition d'une fonction de tri pour les dates, de la plus récente à la plus ancienne
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      // Tri des dates 
+      
+      // Tri des dates extraites en utilisant la fonction de tri définie (antiChrono)
       const datesSorted = [...dates].sort(antiChrono)
+      
       // Verification de l'ordre des dates par rapport "datesSorted"
       expect(dates).toEqual(datesSorted)
     })
@@ -69,16 +78,26 @@ describe("Given I am connected as an employee", () => {
       // Lorsque je clique sur la 'nouvelle note de frais'
       test("Then I am directed to the NewBill page", () => {
         // Alors je suis diréger vers la page NewBill
+
+        // Définit la propriété 'localStorage' de la fenêtre avec une simulation de localStorage
         Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        // Stocke un utilisateur de type 'Employee' dans le localStorage
         window.localStorage.setItem('user', JSON.stringify({
           type: 'Employee'
         }))
+
+        // Crée un élément div avec l'id 'root' et l'ajoute au corps du document
         const root = document.createElement("div")
         root.setAttribute("id", "root")
         document.body.append(root)
+
+        // Initialise le routeur
         router()
+        // Navigue vers la page des factures
         window.onNavigate(ROUTES_PATH.Bills)
+        // Simule un clic sur le bouton 'nouvelle note de frais'
         fireEvent.click(screen.getByTestId('btn-new-bill'))
+        // Vérifie que le formulaire 'form-new-bill' est présent dans le document
         expect(screen.getByTestId('form-new-bill')).toBeTruthy()
       })
     })
@@ -90,21 +109,29 @@ describe("Given I am connected as an employee", () => {
     // Lorsque je clique sur l'icone 'eye'
       test("Then I can open a modal by clicking on the eye icon", () => { 
       // Alors je peux ouvrir une modale en cliquant sur l'icone 'eye' 
+
+      // Mock de la fonction modale de jQuery
       $.fn.modal = jest.fn()
 
+      // Création et ajout de l'élément 'root' au corps du document
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
+
+      // Initialisation du routeur
       router()
+      // Navigation vers la page des factures
       window.onNavigate(ROUTES_PATH.Bills)
 
       // Ajoutez une donnée de facture dans le tableau des factures pour le test.
       const testDataBill = bills[0];
       document.body.innerHTML = BillsUI({ data: [testDataBill] })
+
       // Creation d'un objet Bills pour gérer l'affichage des factures.
       const billsObject = new Bills({
         document, onNavigate, store: mockStore, localStorage: window.localStorage
       })
+
       //Récupérez l'icône "icon-eye"
       const iconEye = screen.getByTestId('icon-eye')
       // Fonction de rappel pour ouvrir la modale
@@ -118,33 +145,9 @@ describe("Given I am connected as an employee", () => {
       })
     })
 
-
-
     // ---------------------------------------------------------------------------- //
     //                            TEST D'INTEGRATION                                   //
     // ---------------------------------------------------------------------------- //
-
-    // describe("Given I am connected as an employee", () => {
-    //   // Lorsque je suis connecté en tant qu'employé 'Employee'
-    //   describe("When I navigate to Dashboard", () => {
-    //     // Quand je navigue vers 'Dashboard'
-    //     test("fetches bills from mock API GET", async () => {
-    //       // Alors j'obtiens les factures de l'API mocké
-
-    //       localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }))
-    //       const root = document.createElement("div")
-    //       root.setAttribute("id", "root")
-    //       document.body.append(root)
-    //       router()
-    //       window.onNavigate(ROUTES_PATH.Bills)
-    //       await waitFor(() => screen.getByText("Nouvelle note de frais"))
-    //       const message = screen.getByText("Nouvelle note de frais")
-    //       // Verification de la presence du texte "Nouvelle note de frais"
-    //       expect(message).toBeTruthy()
-    //     })
-    //   })
-    // })
-    
     describe("Given I am connected as an employee", () => {
       // Lorsque je suis connecté en tant qu'employé
       beforeAll(() => {
